c5d3446bba80a201c5eb940fcb7c175a68dfec28
primetimejas <primetimejas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-01 14:42:37 +0000

    plugin updated for BuddyPress version 2
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-profile-tabs@958693 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.5/bp-profile-tabs.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ *
+ * @package   BP_Profile_Tabs
+ * @author    Jacob Schweitzer <allambition@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://ijas.me
+ * @copyright 2014 Jacob Schweitzer
+ *
+ * Plugin Name:       BuddyPress Profile Tabs
+ * Plugin URI:        http://ijas.me
+ * Description:       Makes the profile groups BuddyPress provides into tabs to better organize and consolidate the profile page. 
+ * Version:           1.5.0
+ * Author:            Jacob Schweitzer
+ * Author URI:        http://ijas.me
+ * Text Domain:       bp-profile-tabs
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( !defined( 'WPINC' ) ) {
+	die;
+}
+
+/*
+ * Load Main Plugin File
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-bp-profile-tabs.php' );
+
+/*
+ * Load Language wrapper function for WPML/Ceceppa Multilingua/Polylang
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'includes/language.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ *
+ */
+register_activation_hook( __FILE__, array( 'BP_Profile_Tabs', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'BP_Profile_Tabs', 'deactivate' ) );
+
+
+add_action( 'plugins_loaded', array( 'BP_Profile_Tabs', 'get_instance' ) );
+
+/*
+ * Load Admin Settings Page
+ */
+if ( is_admin() ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-bp-profile-tabs-admin.php' );
+	add_action( 'plugins_loaded', array( 'BP_Profile_Tabs_Admin', 'get_instance' ) );
+}
---
+++ tags/1.5/uninstall.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package   BP_Profile_Tabs
+ * @author    Jacob Schweitzer <allambition@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://ijas.me
+ * @copyright 2014 Jacob Schweitzer
+ */
+
+// If uninstall not called from WordPress, then exit
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+if ( is_multisite() ) {
+
+	$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
+	
+	if ( $blogs ) {
+
+		foreach ( $blogs as $blog ) {
+			switch_to_blog( $blog[ 'blog_id' ] );
+			
+			delete_option("bp_profile_tabs_theme_option");
+			delete_option("bp_profile_tabs_option");
+
+			restore_current_blog();
+		}
+	}
+} 
+else {
+	delete_option("bp_profile_tabs_theme_option");
+	delete_option("bp_profile_tabs_option");
+}
